b4ab0bdc83fa40e271a7c3e620faab2d98aa370a
the-realest-stu <stuartlindstrom@gmail.com>
2017-11-27 09:17:40 -0500

    user auth, styling, etc

--- public/main.js
+++ public/main.js
@@ -1,67 +1,151 @@
-$( document ).ready(function() {
 
-	$(function () {
+var userdata;
 
-		var username = Math.random() + "_";
+var socket;
 
-		var socket = io();
+function login(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	userdata = {
+		name: profile.getName(),
+		image: profile.getImageUrl(),
+		email: profile.getEmail()
+	};
+	$(this).data('userdata', userdata);
+	if (userdata && socket) {
+		socket.emit('login', userdata);
+	}
+	$('.sign-out-button').remove();
+	$('#login-area').append(
+		$('<button>').addClass("btn btn-primary btn-sm my-2 my-sm-0 sign-out-button").attr('href', "#").text("Sign Out")
+	);
+	$('.g-signin2').prop('disabled', true).hide();
+	refreshMessages();
+}
 
-		$(this).data('username', username);
+function logoff() {
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function () {
+		auth2.disconnect();
+		$('.sign-out-button').remove();
+		userdata = undefined;
+		$(this).data('userdata', userdata);
+		$('.g-signin2').prop('disabled', false).show();
+		refreshMessages();
+	});
+}
 
-		login();
+function refreshMessages() {
+	if (socket) {
+		socket.emit('refresh messages');
+	}
+}
 
-		$('#send-button').on('click', function() {
-		  socket.emit('chat message', $('#m').val());
-		  $('#m').val('');
-		  return false;
-		});
+$(document).ready(function() {
 
-		socket.on('chat message', function(data) {
-		  logMessage(data);
-		});
+	$(window).on('resize', function() {
+		setMessagesHeight();
+		offsetInput();
+	});
 
-		socket.on('user joined', function(data) {
-		  $('#messages').append($('<li>').text(data.username + " has joined"));
-		  $('#active-users').empty();
-		  if (data.activeUsers) {
-				for (var socketID in data.activeUsers) {
-				  $('#active-users').append($('<li>').text(data.activeUsers[socketID]));
-				}
-		  }
-		});
+	$(document).on('click', '.sign-out-button', function() {
+		logoff();
+	});
 
-		socket.on('user left', function(data) {
-		  $('#messages').append($('<li>').text(data.username + " has left"));
-		  $('#active-users').empty();
-		  if (data.activeUsers) {
-				for (var socketID in data.activeUsers) {
-				  $('#active-users').append($('<li>').text(data.activeUsers[socketID]));
-				}
-		  }
-		});
+	function setMessagesHeight() {
+		var extra = $('#messages').outerHeight(true) - $('#messages').height();
+		var messagesHeight = $(window).outerHeight(true) - $('#navbar').outerHeight(true) - $('#input').outerHeight(true) - extra;
+		$('#messages').height(Math.max(messagesHeight, 100));
+	}
 
-		socket.on('logged in', function(data) {
-		  $('#active-users').empty();
-		  if (data.activeUsers) {
-				for (var socketID in data.activeUsers) {
-				  $('#active-users').append($('<li>').text(data.activeUsers[socketID]));
-				}
-		  }
-		});
+	function offsetInput() {
+		var scrollBarWidth = $('#messages').outerWidth() - $('#messages').prop('scrollWidth');
+		var paddingLeft = $('#input').css('padding');
+		paddingLeft = parseInt(paddingLeft.substring(0, paddingLeft.length - 2));
+		$('#input').css('padding-right', (paddingLeft + scrollBarWidth) + "px");
+	}
 
-		function login() {
-		  if (username) {
-				socket.emit('login', username);
-		  }
+	function scrollToNewestMessage() {
+		$('#messages').scrollTop($('#messages').prop('scrollHeight') - $('#messages').height());
+	}
+
+	setMessagesHeight();
+	offsetInput();
+	if (userdata) {
+		$('#login-area').append(
+			$('<button>').addClass("btn btn-primary btn-sm my-2 my-sm-0 sign-out-button").attr('href', "#").text("Sign Out")
+		);
+		$('.g-signin2').prop('disabled', true).hide();
+	}
+
+	socket = io();
+
+	$(this).data('userdata', userdata);
+
+	socket.emit('connect', userdata);
+
+	function sendMessage() {
+		if ($('#m').val().trim() && $('#m').val().trim().length > 0) {
+			if (userdata) {
+				socket.emit('chat message', $('#m').val().trim());
+				$('#m').val('');
+			} else {
+				alert("You must log in in order to send messages!");
+			}
 		}
+		return false;
+	}
 
-		function logMessage(data) {
-		  $('#messages').append($('<li>').text(data.username + ": " + data.message));
+	$('#send-button').on('click', function() {
+		sendMessage();
+	});
+
+	$('#m').on('keyup', function(event) {
+		var keycode = event.keyCode ? event.keyCode : event.which;
+		if (keycode == '13') {
+			sendMessage();
 		}
+	});
 
-  });
+	socket.on('chat message', function(messageData) {
+		logMessage(messageData);
+		$('#messages').scrollTop($('#messages').prop('scrollHeight') - $('#messages').height());
+	});
 
-  const PUBLIC_KEY = 'dc6zaTOxFJmzC';
+	socket.on('user joined', function(data) {
+		
+	});
+
+	socket.on('user left', function(data) {
+		
+	});
+
+	socket.on('logged in', function(data) {
+		
+	});
+
+	socket.on('previous messages', function(messages) {
+		$('#messages').empty();
+		if (messages) {
+			for (var i = 0; i < messages.length; i++) {
+				logMessage(messages[i]);
+			}
+		}
+		$('#messages').scrollTop($('#messages').prop('scrollHeight') - $('#messages').height());
+	});
+
+	function logMessage(messageData) {
+		var message = $('<div>').addClass('row message');
+		message.append($('<div>').addClass('col col-auto').append($('<img>').attr('src', messageData.userdata.image).addClass('profile-image')));
+		var content = $('<div>').addClass('col');
+		content.append($('<h4>').addClass('message-sender' + ((messageData.userdata && userdata && messageData.userdata.email === userdata.email) ? ' current-user' : '')).text(messageData.userdata.name));
+		content.append($('<hr>'));
+		content.append($('<p>').addClass('message-body').text(messageData.message));
+		message.append(content);
+
+		$('#messages').append(message);
+	}
+
+	const PUBLIC_KEY = 'dc6zaTOxFJmzC';
 	const BASE_URL = '//api.giphy.com/v1/gifs/';
 	const ENDPOINT = 'search';
 	const LIMIT = 4;
@@ -76,102 +160,101 @@ $( document ).ready(function() {
 	let currentTimeout;
 
 	let query = {
-	  text: null,
-	  offset: 0,
-	  request() {
-	    return `${BASE_URL}${ENDPOINT}?q=${this.text}&limit=${LIMIT}&rating=${RATING}&offset=${this.offset}&api_key=${PUBLIC_KEY}`;
-	  },
-	  fetch(callback) {
-	    $.getJSON(this.request())
-	      .success(data => {
-	        let results = data.data;
+		text: null,
+		offset: 0,
+		request() {
+			return `${BASE_URL}${ENDPOINT}?q=${this.text}&limit=${LIMIT}&rating=${RATING}&offset=${this.offset}&api_key=${PUBLIC_KEY}`;
+		},
+		fetch(callback) {
+			$.getJSON(this.request())
+			.success(data => {
+				let results = data.data;
 
-	        if (results.length) {
-	          let url = results[0].images.downsized.url;
-	          console.log(results);
-	          callback(url);
-	        } else {
-	          callback('');
-	        }
-	      })
-	      .fail(error => {
-	        console.log(error);
-	      }
-	    );
-	  }
+				if (results.length) {
+					let url = results[0].images.downsized.url;
+					console.log(results);
+					callback(url);
+				} else {
+			  		callback('');
+				}
+			})
+			.fail(error => {
+				console.log(error);
+			});
+		}
 	}
 
 	function buildImg(src = '//giphy.com/embed/xv3WUrBxWkUPC', classes = 'gif hidden') {
-	  return `<img src="${src}" class="${classes}" alt="gif" />`;
+		return `<img src="${src}" class="${classes}" alt="gif" />`;
 	}
 
 	$clear.on('click', e => {
-	  $queryInput.val('');
-	  $inputWrapper.removeClass('active').addClass('empty');
-	  $('.gif').addClass('hidden');
-	  $loader.removeClass('done');
-	  $button.removeClass('active');
+		$queryInput.val('');
+		$inputWrapper.removeClass('active').addClass('empty');
+		$('.gif').addClass('hidden');
+		$loader.removeClass('done');
+		$button.removeClass('active');
 	});
 
 	$button.on('click', e => {
-	  query.offset = Math.floor(Math.random() * 25);
+		query.offset = Math.floor(Math.random() * 25);
 
-	  query.fetch(url => {
-	    if (url.length) {
-	      $resultWrapper.html(buildImg(url));
+		query.fetch(url => {
+			if (url.length) {
+				$resultWrapper.html(buildImg(url));
 
-	      $button.addClass('active');
-	    } else {
-	      $resultWrapper.html(`<p class="no-results hidden">No Results found for <strong>${query.text}</strong></p>`);
+				$button.addClass('active');
+			} else {
+				$resultWrapper.html(`<p class="no-results hidden">No Results found for <strong>${query.text}</strong></p>`);
 
-	      $button.removeClass('active');
-	    }
+				$button.removeClass('active');
+			}
 
-	    $loader.addClass('done');
-	    currentTimeout = setTimeout(() => {
-	      $('.hidden').toggleClass('hidden');
-	    }, 1000);
-	  });
+			$loader.addClass('done');
+			currentTimeout = setTimeout(() => {
+				$('.hidden').toggleClass('hidden');
+			}, 1000);
+		});
 	});
 
 	$queryInput.on('keyup', e => {
-	  let key = e.which || e.keyCode;
-	  query.text = $queryInput.val();
-	  query.offset = Math.floor(Math.random() * 25);
+		let key = e.which || e.keyCode;
+		query.text = $queryInput.val();
+		query.offset = Math.floor(Math.random() * 25);
 
-	  if (currentTimeout) {
-	    clearTimeout(currentTimeout);
-	    $loader.removeClass('done');
-	  }
+		if (currentTimeout) {
+			clearTimeout(currentTimeout);
+			$loader.removeClass('done');
+		}
 
-	  currentTimeout = setTimeout(() => {
-	    currentTimeout = null;
-	    $('.gif').addClass('hidden');
+		currentTimeout = setTimeout(() => {
+			currentTimeout = null;
+			$('.gif').addClass('hidden');
 
-	    if (query.text && query.text.length) {
-	      $inputWrapper.addClass('active').removeClass('empty');
+			if (query.text && query.text.length) {
+				$inputWrapper.addClass('active').removeClass('empty');
 
-	      query.fetch(url => {
-	        if (url.length) {
-	          $resultWrapper.html(buildImg(url));
+				query.fetch(url => {
+					if (url.length) {
+						$resultWrapper.html(buildImg(url));
 
-	          $button.addClass('active');
-	        } else {
-	          $resultWrapper.html(`<p class="no-results hidden">No Results found for <strong>${query.text}</strong></p>`);
+						$button.addClass('active');
+					} else {
+						$resultWrapper.html(`<p class="no-results hidden">No Results found for <strong>${query.text}</strong></p>`);
 
-	          $button.removeClass('active');
-	        }
+						$button.removeClass('active');
+					}
 
-	        $loader.addClass('done');
-	        currentTimeout = setTimeout(() => {
-	          $('.hidden').toggleClass('hidden');
-	        }, 100);
-	      });
-	    } else {
-	      $inputWrapper.removeClass('active').addClass('empty');
-	      $button.removeClass('active');
-	    }
-	  }, 1000);
+					$loader.addClass('done');
+					currentTimeout = setTimeout(() => {
+						$('.hidden').toggleClass('hidden');
+					}, 100);
+				});
+			} else {
+				$inputWrapper.removeClass('active').addClass('empty');
+				$button.removeClass('active');
+			}
+		}, 1000);
 	});
 
 });
---
+++ server.js
@@ -0,0 +1,79 @@
+
+var express = require('express');
+var http = require('http');
+var socketIO = require('socket.io');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// Create the express app, the http server, and socket.io server
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Handle routing automagically using express and the 'path' dependency
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start server on predefined port
+server.listen(PORT, function() {
+    console.log('listening on *:' + PORT);
+});
+
+// The actual chatroom
+var MAX_SAVED_MESSAGES = 128;
+var messages = [];
+
+io.on('connection', function(socket) {
+
+    socket.on('chat message', function(msg) {
+        var messageData = {
+            userdata: socket.userdata,
+            id: socket.id,
+            message: msg
+        };
+
+        // Relay the message to all clients
+        io.emit('chat message', messageData);
+
+        // Remove old saved messages to make room for this one
+        while (messages.length >= MAX_SAVED_MESSAGES) {
+            messages.shift();
+        }
+        // Save the message
+        messages.push(messageData);
+    });
+
+    socket.on('refresh messages', function() {
+        socket.emit('previous messages', messages);
+    });
+
+    socket.on('login', function(userdata) {
+        // Store the username in the socket session for this client
+        socket.userdata = userdata;
+
+        // Tell the client that it successfully logged in
+        socket.emit('logged in', {
+            userdata: socket.userdata
+        });
+        // Tell all clients that this client has joined
+        socket.broadcast.emit('user joined', {
+            userdata: socket.userdata
+        });
+    });
+
+    socket.on('disconnect', function() {
+        if (socket.userdata) {
+            // Tell all clients that this client has left
+            socket.broadcast.emit('user left', {
+                userdata: socket.userdata
+            });
+        }
+    });
+
+    // Send the client the messages from before they connected
+    socket.emit('previous messages', messages);
+
+});
